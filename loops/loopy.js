b7a203fcfe0d44f66b546e25685a3d590d0519ac
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-10-15 23:10:15 -0700

    Loop examples

---
+++ loops/loopy.js
@@ -0,0 +1,13 @@
+for(var i = 0; i < 10; i++){
+    console.log("I", i);
+}
+
+var j = 5;
+while(j < 10){
+    console.log("J", j);
+    j++;
+}
+
+for(var k = 10; k >= 0; k--){
+    console.log("K", k);
+}
