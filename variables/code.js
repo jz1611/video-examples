10fad2a9d554e9da24e88bf773b365ff1e485822
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-10-15 21:35:34 -0700

    Variable intro

---
+++ variables/code.js
@@ -0,0 +1,12 @@
+// KEYWORD LABEL = VALUE;
+// KEYWORD LABEL;
+// LABEL = VALUE;
+var name = "Jeff";
+var email;
+email = "hello@hello.com";
+
+// let, const;
+const hello = "Hello";
+console.log(email, name, hello);
+
+let age = 999;
