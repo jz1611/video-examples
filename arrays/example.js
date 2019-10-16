a7cb552ebcaf2a90d3f9edbb12bedf7a116c0340
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-10-15 22:09:38 -0700

    Updated array intro file.

---
+++ arrays/example.js
@@ -0,0 +1,21 @@
+// What is an array?
+// How to create an array (syntax)?
+// What can you store in an array?
+// How to access values in an array?
+// push, pop, shift, unshift, splice, slice
+
+
+let myFriends = ['Dave', 'Dan', 'Nate'];
+let copy = myFriends.slice(0,2);    // Slice can copy array using given index values.
+myFriends.push('Greg');             // Adds to end of array.
+myFriends.pop();                    // Removes from end of array.
+myFriends.shift();                  // Removes from front of array.
+myFriends.unshift('Dave');          // Adds to fron of array.
+myFriends.splice(1, 1, 'Eric')      // Removes items including and between given index values. Can add in values at spot.
+
+
+
+
+
+myFriends;
+copy;
